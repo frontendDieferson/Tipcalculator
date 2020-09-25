ef9b8ea92b2ea80ee4837dad6ff382b9afa4ca4f
Dieferson <diefersonfrontenddeveloper@gmail.com>
2020-09-24 20:30:05 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,109 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Container, Image, Footer } from './styles';
+
+
+
+
+
+const Title = styled.h1`
+  font-family: 'Roboto', sans-serif;
+  color: #FFF;
+  margin: auto;
+
+  @media (max-width: 414px) {
+    flex-direction: column;
+    
+
+    }
+  
+
+`;
+
+const Paragraph = styled.p`
+  font-family: 'Roboto', sans-serif;
+  color: #FFF;
+  margin: auto;
+
+  @media (max-width: 414px) {
+    flex-direction: column;
+   
+
+    }
+  
+  
+
+`;
+
+const Result = styled.h3`
+  font-family: 'Roboto', sans-serif;
+  color: #FFF;
+  margin: auto;
+
+  @media (max-width: 414px) {
+    flex-direction: column;
+   
+
+    }
+  
+`;
+
+const Input = styled.input`
+  width:400px;
+  height:30px;
+  font-size:16px;
+  padding:10px;
+  border:1px solid #151A6A;
+  border-radius: 8px;
+  color: #151A6A;
+  font-family: 'Roboto', sans-serif;
+  margin: 8px;
+  
+  @media (max-width: 414px) {
+    flex-direction: column;
+    width:280px;
+
+    }
+
+`;
+
+
+
+
+function App() {
+
+  const [pct, setPct] = useState(10);
+  const [conta, setConta] = useState(0);
+
+  return (
+
+    <Container>
+      <Image src="./assets/dinheiro.svg" />
+      <Title>Calculadora de Gorjeta</Title>
+
+      <Paragraph>Quanto deu a conta?</Paragraph>
+      <Input type="number" value={conta} onChange={(e)=>setConta(parseFloat(e.target.value))} />
+      
+      <Paragraph>Qual a porcentagem de gorjeta?</Paragraph>
+      <Input type="number" value={pct} onChange={(e)=>setPct(parseFloat(e.target.value))} />
+      
+      <hr/>
+
+      {conta > 0 &&
+        <Container>
+          <Paragraph>Sub-total: R$ {conta}</Paragraph>
+          <Paragraph>Gorjeta ({pct}%): R$ {(pct/100) * conta}</Paragraph>
+          <Result>Total: R$ {conta + ((pct/100) * conta)}</Result>
+        </Container>
+      }
+      
+      <Footer>
+        <p>Desenvolvido por Dieferson Soares</p>
+      </Footer>
+    </Container>
+
+    
+  );
+}
+
+export default App;
---
+++ src/styles.js
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+
+export const Image = styled.img`
+    display: grid;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    margin: auto;
+    align-self: center;
+    width: 200px;
+    height: 200px;
+
+    @media (max-width: 414px) {
+    flex-direction: column;
+   
+    margin: auto;
+
+    }
+`;
+
+export const Container = styled.div`
+    
+    display: grid;
+    
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+
+    align-self: center;
+    
+    background: #151A6A;
+    margin: 1%;
+    padding: 3%;
+    
+    @media (max-width: 414px) {
+    flex-direction: column;
+    
+
+    }
+    
+    
+`;
+
+export const Footer = styled.p`
+
+    display: grid;
+    
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    align-self: center;
+
+    color: #FFF;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 300;
+    margin-top: 80px;
+`;
